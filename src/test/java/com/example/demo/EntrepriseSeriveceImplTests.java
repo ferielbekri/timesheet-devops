584daa67a26ed1efb50e90a2a3b734ff2ecf1554
feriel bekri <feriel.bekri@esprit.tn>
2021-11-14 18:10:40 +0100

    modification ++++++++++

--- src/test/java/com/example/demo/EntrepriseSeriveceImplTests.java
+++ src/test/java/com/example/demo/EntrepriseSeriveceImplTests.java
@@ -26,7 +26,7 @@ public class EntrepriseSeriveceImplTests {
 	@Order(1)
 	public void testRetrieveAllEntreprise() {
 		List<Entreprise> ListeEntreprise = entrepriseService.retrieveAllEntreprise();
-		Assertions.assertEquals(5, ListeEntreprise.size());;
+		Assertions.assertEquals(6, ListeEntreprise.size());;
 		
 	}
 	
@@ -57,7 +57,7 @@ public class EntrepriseSeriveceImplTests {
 	@Test
 	@Order(4)
 	public void testDeleteEntreprise(){
-		entrepriseService.deleteEntreprise(41);
+		entrepriseService.deleteEntreprise(42);
 		//Assertions.assertNull(entrepriseService.retrieveEntreprise(9));
 		
 	}
